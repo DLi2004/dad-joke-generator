df347e22da82d70011fbc51120e6e0164e22fccf
DLi2004 <danielmli2023@gmail.com>
2023-06-14 13:36:47 -0400

    finish basic layout

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 This is a simple dad joke generator.
-
-### TODO
-- [ ] Display 
--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('Dad Jokes Starter');
+const url = 'https://icanhazdadjoke.com/';
+
+const btn = document.getElementById("joke-btn");
+const jokeText = document.getElementById("joke-text");
+
+// Add event listener to button
+btn.addEventListener('click', () => {
+    fetchDadJoke();
+});
+
+const fetchDadJoke = async () => {
+    try {
+        // Get response from website
+        const response = await fetch(url, {
+            headers: {
+                Accept: 'application/json',
+                'User-Agent': 'Learning application: (https://github.com/DLi2004/dad-joke-generator)',
+            },
+        });
+
+        // Check for error
+        if (!response.ok) {
+            throw new Error(' error');
+        }
+
+        // Change joke text
+        const data = await response.json();
+        jokeText.textContent = data.joke;
+    } catch (error) {
+        console.log(error.message);
+        jokeText.textContent = 'There was an error.';
+    }
+};
+
+// Initial dad joke
+fetchDadJoke();
